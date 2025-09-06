b1bcb0c243c0c2edf1dbfce0a1ea5206c5713147
AZCI456 <acoelhoirani@student.unimelb.edu.au>
2025-09-07 08:33:00 +1000

    Redundent file cleanup as-well-as comments in index file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// consolidates the function into one script to be imported into whatever
+//  necessary application case
 export { runOpenAIModeration } from "./runOpenAIModeration.js";
 export { runPerspective } from "./runPerspective.js";
 export { runGSafeBrowsing } from "./runGSafeBrowsing.js";
